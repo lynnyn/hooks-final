0b98a79c1fe2c70d6e012b3c0492b96199b8be60
Lynn Yuan <lynnyuann@gmail.com>
2020-06-16 09:06:38 +1200

    use Ref

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 import ParentComponent from './components/ParentComponent';
 import Counter from './components/Counter';
+import FocusInput from './components/FocusInput';
+import ClassTimer from './components/ClassTimer';
+import HookTimer from './components/HookTimer';
 
 
 function App() {
   return (
     <div className="App">
       {/* <ParentComponent /> */}
-      <Counter />
+      {/* <Counter /> */}
+      {/* <FocusInput /> */}
+      {/* <ClassTimer /> */}
+      <HookTimer />
     </div>
   );
 }
