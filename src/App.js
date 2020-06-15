40be624085d1dca0224eaa871fd185cb18c7973e
Lynn Yuan <lynnyuann@gmail.com>
2020-06-15 15:58:07 +1200

    useMemo hook

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ParentComponent from './components/ParentComponent';
+import Counter from './components/Counter';
 
 
 function App() {
   return (
     <div className="App">
-      <ParentComponent />
+      {/* <ParentComponent /> */}
+      <Counter />
     </div>
   );
 }
